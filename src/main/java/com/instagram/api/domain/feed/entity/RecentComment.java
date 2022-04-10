46aaa7a5b3994b49c01e3b8ffc55b69ad1564fe0
jsh15932 <jsh15932@jsh15932ui-MacBookPro.local>
2022-04-10 17:30:31 +0900

    Add Entity:Bookmark

---
+++ src/main/java/com/instagram/api/domain/feed/entity/RecentComment.java
@@ -0,0 +1,40 @@
+package com.instagram.api.domain.feed.entity;
+
+import com.instagram.api.domain.member.entity.Member;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Table(name = "recent_comments")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RecentComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "recent_comment_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    @Builder
+    public RecentComment(Member member, Post post, Comment comment) {
+        this.member = member;
+        this.post = post;
+        this.comment = comment;
+    }
+}
