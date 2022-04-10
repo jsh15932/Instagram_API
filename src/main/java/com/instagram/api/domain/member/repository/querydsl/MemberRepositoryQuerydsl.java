fdfd6d5c34921862e6020527271236aab550da0a
jsh15932 <jsh15932@jsh15932ui-MacBookPro.local>
2022-04-11 01:02:34 +0900

    Add QueryDSL:Member Repository Query DSL

--- src/main/java/com/instagram/api/domain/member/repository/querydsl/MemberRepositoryQuerydsl.java
+++ src/main/java/com/instagram/api/domain/member/repository/querydsl/MemberRepositoryQuerydsl.java
@@ -1,6 +1,14 @@
 package com.instagram.api.domain.member.repository.querydsl;
 
+import com.instagram.api.domain.member.dto.MiniProfileResponse;
+import com.instagram.api.domain.member.dto.UserProfileResponse;
+import com.instagram.api.domain.member.entity.Member;
+
+import java.util.List;
+
 public interface MemberRepositoryQuerydsl {
 
-
+    UserProfileResponse getUserProfile(Long loginUserId, String username);
+    MiniProfileResponse getMiniProfile(Long loginUserId, String username);
+    List<Member> findAllByUsernames(List<String> usernames);
 }
