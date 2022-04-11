adadb73a5a24a81c7a16a05137a0031c0e455c0c
jsh15932 <jsh15932@noutecompany.com>
2022-04-11 21:36:52 +0900

    Add Service:Member Register Service

--- src/main/java/com/instagram/api/domain/member/service/MemberAuthService.java
+++ src/main/java/com/instagram/api/domain/member/service/MemberAuthService.java
@@ -3,11 +3,14 @@ package com.instagram.api.domain.member.service;
 import com.instagram.api.domain.member.dto.RegisterRequest;
 import com.instagram.api.domain.member.entity.Member;
 import com.instagram.api.domain.member.repository.MemberRepository;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Slf4j
 @Service
@@ -16,6 +19,7 @@ public class MemberAuthService {
 
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Transactional
     public boolean register(RegisterRequest registerRequest) {
@@ -25,5 +29,10 @@ public class MemberAuthService {
 
         String username = registerRequest.getUsername();
         Member member = registerRequest.convert();
+        String encryptedPassword = bCryptPasswordEncoder.encode(member.getPassword());
+        member.setEncryptedPassword(encryptedPassword);
+        memberRepository.save(member);
+
+        return true;
     }
 }
